bad3b9a4cb732d1bde1731c3aa5d057712855211
Waldeck Lindoso Jr <wlindoso@gmail.com>
2022-09-07 11:45:19 -0300

    update
    Need to fix entity validation failure.

--- src/main/java/com/son/RestAPI/models/Product.java
+++ src/main/java/com/son/RestAPI/models/Product.java
@@ -6,6 +6,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+/** Validation Constraints
+ * @NotNull: to say that a field must not be null.
+ * @NotEmpty: to say that a list field must not empty.
+ * @NotBlank: to say that a string field must not be the empty string (i.e. it must have at least one character).
+ * @Min and @Max: to say that a numerical field is only valid when it’s value is above or below a certain value.
+ * @Pattern: to say that a string field is only valid when it matches a certain regular expression.
+ * @Email: to say that a string field must be a valid email address.
+ */
 
 @Entity
 public class Product {
@@ -13,8 +28,19 @@ public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotEmpty(message = "Can not be empty.") // Para dizer que um campo não deve ser nulo.
+	@NotBlank(message = "Can not be blank.") // Para dizer que um campo de lista não deve ficar vazio
+	@Min(value = 4) // dizer que um campo numérico só é válido quando seu... 
+	@Max(value = 255) // ...valor está acima ou abaixo de um determinado valor.
+	@Size(min = 4, max = 255) // @Size => sempre que usar String
 	private String name;
+	
+	@NotEmpty(message = "Can not be empty.")
+	@Min(value = 0) // @Min e @Max => sempre que usar Inteiro
+	@Max(value = 1000)
 	private Integer qtd;
+	
 	private Date dateCreated;
 
 	public Product() {
@@ -25,6 +51,13 @@ public class Product {
 		this.qtd = qtd;
 	}
 
+	@PrePersist
+	public void onPrePesist() {
+		if (this.dateCreated == null) {
+			this.dateCreated = new Date();
+		}
+	}
+	
 	public Long getId() {
 		return id;
 	}
@@ -59,9 +92,8 @@ public class Product {
 
 	@Override
 	public String toString() {
-		return "{id: " + this.id + ", name: " + this.name + ", qtd: " + this.qtd + ", dateCreated: " + this.dateCreated + "}";
+		return "{id: " + this.id + ", name: " + this.name + ", qtd: " + this.qtd + ", dateCreated: " + this.dateCreated
+				+ "}";
 	}
-	
-	
 
 }
--- src/main/java/com/son/RestAPI/resources/ProductResource.java
+++ src/main/java/com/son/RestAPI/resources/ProductResource.java
@@ -2,10 +2,14 @@ package com.son.RestAPI.resources;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,11 +18,16 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.son.RestAPI.models.Product;
 import com.son.RestAPI.services.ProductService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "API REST - Model Product")
 @RestController
 @RequestMapping("/products")
 public class ProductResource {
@@ -30,35 +39,60 @@ public class ProductResource {
 		this.productService = productService;
 	}
 
-	@GetMapping
+	@ApiOperation(value = "Find all products in database.")
+	@GetMapping(produces = "application/json")
 	@ResponseBody // caso precisa retornar algo
-	public List<Product> findAll() {
-		return this.productService.findAll();
+	public ResponseEntity<?> findAll() {
+		List<Product> list = this.productService.findAll();
+		return new ResponseEntity<List>(list, HttpStatus.OK);
 	}
 
+	@ApiOperation(value = "Find by ID in database.")
 	@GetMapping(value = "/{id}") // Recebe na URL, o id do produto a buscar.
 	@ResponseBody
-	public Optional<Product> findById(@PathVariable(value = "id") Long id) {
-		return this.productService.findById(id);
+	public ResponseEntity<?> findById(@PathVariable(value = "id") Long id) {
+		Optional<Product> product = this.productService.findById(id);
+		return new ResponseEntity<Optional<Product>>(product, HttpStatus.OK);
 	}
 
+	@ApiOperation(value = "Create new product in database.")
 	@PostMapping
 	@ResponseBody
-	public Product create(@RequestBody Product product) {
-		return this.productService.create(product);
+	@ResponseStatus(code = HttpStatus.CREATED) // mais utilizado dentro do Spring // CREATED - 201
+	public ResponseEntity<?> create(@Valid @RequestBody Product product, Errors errors) {
+		if (!errors.hasErrors()) {
+			Product productCreated = this.productService.create(product);
+//			return ResponseEntity.ok(productCreated);
+			return new ResponseEntity<Product>(productCreated, HttpStatus.CREATED);
+		}
+		return ResponseEntity.badRequest().body(
+				errors.getAllErrors().stream().map(msg -> msg.getDefaultMessage()).collect(Collectors.joining(",")));
 	}
+//	@PostMapping
+//	@ResponseBody
+//	@ResponseStatus(code = HttpStatus.CREATED) // mais utilizado dentro do Spring // CREATED - 201
+//	public Product create(@Valid @RequestBody Product product) {
+//		return this.productService.create(product);
+//	}
 
+	@ApiOperation(value = "Update product in database.")
 	@PutMapping(value = "/{id}") // Recebe na URL, o id do produto a atualizar.
 	@ResponseBody
-	public Product update(@PathVariable(value = "id") Long id, @RequestBody Product product) {
-		return this.productService.update(id, product);
+	public ResponseEntity<?> update(@PathVariable(value = "id") Long id, @Valid @RequestBody Product product,
+			Errors errors) {
+		if (!errors.hasErrors()) {
+			Product productUpdated = this.productService.update(id, product);
+			return new ResponseEntity<Product>(productUpdated, HttpStatus.OK);
+		}
+		return ResponseEntity.badRequest().body(
+				errors.getAllErrors().stream().map(msg -> msg.getDefaultMessage()).collect(Collectors.joining(",")));
 	}
 
+	@ApiOperation(value = "Delete product in database.")
 	@DeleteMapping(value = "/{id}") // Recebe na URL, o id do produto a deletar.
-	@ResponseBody
-	public void delete(@PathVariable(value = "id") Long id, HttpServletResponse response) {
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable(value = "id") Long id) {
 		this.productService.delete(id);
-		response.setStatus(HttpServletResponse.SC_NO_CONTENT); // Code 204
 	}
 
 }
